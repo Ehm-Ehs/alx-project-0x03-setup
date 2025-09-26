9088452bb2cc273cbcc722898f11bf0f401de217
Ehm-Ehs <salamimariam239@gmail.com>
2025-09-26 15:19:03 +0100

    fix: fixed ui based on review

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,9 +1,4 @@
-interface ButtonProps {
-  buttonLabel: string;
-  buttonSize?: string;
-  buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
-  action?: () => void;
-}
+import { ButtonProps } from "@/interface";
 
 const Button = ({
   buttonLabel,
@@ -23,7 +18,7 @@ const Button = ({
   return (
     <button
       onClick={action}
-      className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg hover:opacity-80 transition duration-300 text-white`}
+      className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg hover:${backgroundColorClass}/50 transition duration-300 text-white`}
     >
       {buttonLabel}
     </button>
